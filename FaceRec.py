13364e359f4f544d3db0e8ff07a3ef5df58e2381
ShaqPhil-Russ <133050177+ShaqPhil-Russ@users.noreply.github.com>
2023-05-12 13:19:17 -0400

    Add files via upload

---
+++ FaceRec.py
@@ -0,0 +1,43 @@
+imagePath = ''
+cascPath = 'haarcascade_frontalface_default.xml'
+
+import cv2
+
+image = cv2.imread(imagePath)
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Gray Scale Image", gray)
+cv2.waitKey(0)
+
+# Just to see 
+# we can work the image to 'help' the algorithm
+clahe = cv2.createCLAHE(
+    clipLimit=3,  
+    tileGridSize=(10,10))
+gray = clahe.apply(gray)
+
+cv2.imshow("After apply CLAHE", gray)
+cv2.waitKey(0)
+
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.2,
+    minNeighbors=5,
+    minSize=(30,30),
+     flags=cv2.CASCADE_SCALE_IMAGE
+)
+
+print(f"Found {len(faces)} faces!")
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(
+        image, 
+        (x, y), 
+        (x+w, y+h), 
+        (0, 255, 0), 2
+    )
+
+cv2.imshow("Faces found", image)
+cv2.waitKey(0)
